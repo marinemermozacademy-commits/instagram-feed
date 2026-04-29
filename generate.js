077d18f4de5bcf8e5d0c8a365164f1bebea3fdf5
marinemermozacademy-commits <marine.mermozacademy@gmail.com>
2026-04-29 16:47:03 +0200

    Update generate.js

--- generate.js
+++ generate.js
@@ -1,12 +1,12 @@
 const fs = require("fs");
 
-const FEED_URL = "https://feeds.behold.so/ggzF9fLF0zKUC5DAK1q2";
+const FEED_URL = `https://feeds.behold.so/ggzF9fLF0zKUC5DAK1q2?t=${Date.now()}`;
 
 async function main() {
   const res = await fetch(FEED_URL);
   const data = await res.json();
 
-  const posts = data.posts.slice(0, 9);
+  const posts = data.posts.slice(0, 6);
 
   const html = `
 <!DOCTYPE html>
